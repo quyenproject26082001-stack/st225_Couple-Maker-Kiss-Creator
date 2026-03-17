033dcd08242a25a992f070db9cc9ce64f4eec50d
unknown <quyenle26082001@gmail.com>
2026-03-17 18:40:18 +0700

    done

---
+++ app/src/main/java/com/temp/CosplayRandomActivity.kt
@@ -0,0 +1,27 @@
+package com.temp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.temp.ui.home.DataViewModel
+import com.temp.ui.random_character.RandomCharacterViewModel
+
+class CosplayRandomActivity : AppCompatActivity() {
+
+    private val viewModel: RandomCharacterViewModel by viewModels()
+    private val dataViewModel: DataViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cosplay_random2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
